adcba32f0a2d2111a0ad413e130ae8fd69bccc75
Shiellyn Ferreira <brunashiellyn@gmail.com>
2021-11-26 21:09:01 -0300

    home do blog em react teste

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from './paginas/home/Home';
+import "./App.css";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo" >Home</h1>
+      <img
+        src="https://images.unsplash.com/photo-1637928114342-05b15ee4034e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        alt="imagem tela inicial" className="imagem"
+      />
+    </>
+  );
+}
+
+export default Home;
